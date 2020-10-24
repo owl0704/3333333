92b9de926f18860e78a40b732170d2e7ea0a2674
lizhixiang <3045055246@qq.com>
2020-10-24 23:14:19 +0700

    Exercise 3 completed

--- components/samples/03_props.js
+++ components/samples/03_props.js
@@ -1,17 +1,33 @@
 import React from "react";
 
 const Demo = (props) => {
-    return (
-      <div >
-        <h2>{`${props.heading} Languages`}</h2>
-        <ul> 
-          <li>{props.languages[0]}</li>
-          <li>{props.languages[1]} </li>
-          <li>{props.languages[2]} </li>
-        </ul>
-      </div>
-    );
-  
-}
+  return (
+    <div>
+      <h2>{`${props.course} Languages`}</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{props.modules[0].name}</td>
+            <td>{props.modules[0].noLectures}</td>
+            <td>{props.modules[0].noPracticals}</td>
+          </tr>
+          <tr>
+            <td>{props.modules[1].name}</td>
+            <td>{props.modules[1].noLectures}</td>
+            <td>{props.modules[1].noPracticals}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
 
 export default Demo
