2c6f326e837f85ed3098edeb075dbbe9c596123c
lizhixiang <3045055246@qq.com>
2020-10-24 23:24:59 +0700

    Exercise 3 completed

---
+++ components/exercises/03_props.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "../../node_modules/bootstrap/dist/css/bootstrap.css";
+const Demo = (props) => {
+    return (
+      <div>
+        <h2>{`${props.course} Languages`}</h2>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>No lectures</th>
+              <th>No practicals</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{props.modules[0].name}</td>
+              <td>{props.modules[0].noLectures}</td>
+              <td>{props.modules[0].noPracticals}</td>
+            </tr>
+            <tr>
+              <td>{props.modules[1].name}</td>
+              <td>{props.modules[1].noLectures}</td>
+              <td>{props.modules[1].noPracticals}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+  
+  export default Demo;
+  
